d6c51c1ce1ff0a33134ed3109e53863241025d8b
Patrick McDonald <paddymcdonald@gmail.com>
2023-10-01 14:46:29 +0100

    Remove boilerplate comment

--- LegacyWcfService/UserService.svc.cs
+++ LegacyWcfService/UserService.svc.cs
@@ -4,8 +4,6 @@ using LegacyWcfService.Tracing;
 
 namespace LegacyWcfService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select UserService.svc or UserService.svc.cs at the Solution Explorer and start debugging.
     public class UserService : IUserService
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
